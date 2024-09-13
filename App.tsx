24f663b8c4e78717d246ae89c1b81e977c9dcfaf
UtkarshDeoli <utkarsh.deoli@gmail.com>
2024-09-14 00:56:59 +0530

    added screen landing

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import { styled } from 'nativewind';
+import {NavigationContainer} from '@react-navigation/native';
 
 import {
   SafeAreaView,
@@ -8,7 +9,9 @@ import {
   StatusBar,
   Text,
   useColorScheme,
-  Image
+  Image,
+  View,
+  Pressable,
 } from 'react-native';
 
 const SafeAreaViewStyled = styled(SafeAreaView);
@@ -16,13 +19,24 @@ const ScrollViewStyled = styled(ScrollView);
 const StatusBarStyled = styled(StatusBar);
 const TextStyled = styled(Text);
 const ImageStyled = styled(Image);
+const StyledView = styled(View);
+const StyledButton = styled(Pressable);
 
 export default function App() {
+  function alert(arg0: string): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
-    <SafeAreaViewStyled className="flex-1 justify-center items-center bg-gray-100">
-      <ScrollViewStyled className="bg-gray-100">
-        <ImageStyled src='/assets/images/owl.jpg'></ImageStyled>
-      </ScrollViewStyled>
+  <NavigationContainer>
+    <SafeAreaViewStyled className="flex-1 justify-center items-center bg-[#415e70]">
+      <StyledView className=' flex justify-center items-center'>
+        <ImageStyled className='h-80 w-80 flex ' source={require('./assets/images/owl2.jpg')} />
+      </StyledView>
+      <StyledButton className='border-slate-800 border px-4 py-2 rounded-lg bg-blue-500' onPress={() => alert('Simple Button pressed')} ><TextStyled className='text-slate-900 text-xl
+      '>Continue</TextStyled></StyledButton>
     </SafeAreaViewStyled>
+  </NavigationContainer>
+    
   );
 }
